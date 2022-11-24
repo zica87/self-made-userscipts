9e486a447994e6fc879aeb7c7de5d9571aff9483
zica87 <59327276+zica87@users.noreply.github.com>
2022-11-24 17:11:48 +0800

    Create 樂詞網自動選擇顯示50筆.js

---
+++ 樂詞網自動選擇顯示50筆.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         樂詞網自動選擇顯示 50 筆
+// @namespace    https://github.com/zica87
+// @version      0.1
+// @description  每頁筆數自動選擇 50
+// @author       zica
+// @match        https://terms.naer.edu.tw/search/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const a = document.getElementsByClassName('form-select-sm')[0];
+    // No.2 == 50
+    a.selectedIndex = 2;
+    a.children[2].dispatchEvent(new Event('change', {bubbles: true}));
+})();
