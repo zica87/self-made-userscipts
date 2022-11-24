33f2462cdcf231ff70fe046566327ee041a5797d
zica87 <59327276+zica87@users.noreply.github.com>
2022-11-24 17:32:55 +0800

    Create 自動關閉知乎登入提示框.js

---
+++ 自動關閉知乎登入提示框.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         自動關閉知乎登入提示框
+// @namespace    https://github.com/zica87
+// @version      0.1
+// @description  自動關閉知乎登入提示框（擋在正中間那個）
+// @author       zica
+// @match        https://zhuanlan.zhihu.com/p/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const obs = new MutationObserver((recs, instance)=>{
+        const X = document.getElementsByClassName('Modal-closeButton')[0];
+        if (X){
+            instance.disconnect();
+            X.click();
+        }
+    });
+    obs.observe(document.body, {
+        childList: true
+    });
+})();
