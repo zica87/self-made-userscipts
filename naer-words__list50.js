5ea97ae0cb088dc2fd38e08fc201ba4ce84b4e35
zica <9918800@gmail.com>
2022-11-24 22:05:43 +0800

    [feat] 新增筆數註解

--- naer-words__list50.js
+++ naer-words__list50.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         樂詞網自動選擇顯示 50 筆
 // @namespace    https://github.com/zica87
-// @version      0.1
+// @version      0.1.1
 // @description  每頁筆數自動選擇 50
 // @author       zica
 // @match        https://terms.naer.edu.tw/search/*
@@ -13,7 +13,11 @@
     'use strict';
 
     const a = document.getElementsByClassName('form-select-sm')[0];
-    // No.2 == 50
-    a.selectedIndex = 2;
-    a.children[2].dispatchEvent(new Event('change', {bubbles: true}));
+    // 0 = 10 筆（即預設值）
+    // 1 = 20 筆
+    // 2 = 50 筆
+    // 3 = 100 筆
+    const selectIndex = 2;
+    a.selectedIndex = selectIndex;
+    a.children[selectIndex].dispatchEvent(new Event('change', {bubbles: true}));
 })();
