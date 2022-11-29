6b2c2e0599839a50b82ee49b4a652ac90f463a59
zica <9918800@gmail.com>
2022-11-29 11:06:25 +0800

    [add] zerojudge 隱藏標籤

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 |[樂詞網自動選擇顯示50筆](./naer-words__list50.js)|[安裝](https://greasyfork.org/zh-TW/scripts/455352-樂詞網自動選擇顯示-50-筆)|每頁筆數自動選擇 50|
 |[顯示動畫瘋封面 & 視覺圖](./ani-gamer__show-cover.js)|[安裝](https://greasyfork.org/zh-TW/scripts/454906-顯示動畫瘋封面-視覺圖)|在動畫瘋網站顯示該集封面 & 視覺圖|[範例](https://ani.gamer.com.tw/animeVideo.php?sn=31941)|
 |[自動關閉知乎登入提示框](./zhihu__close-login-prompt.js)|待上架|自動關閉知乎登入提示框（擋在正中間那個）|[範例](https://zhuanlan.zhihu.com/p/341637818)|
+|[zerojudge 隱藏標籤](./zerojudge__hide-tags.js)|待上架|不顯示題目標籤，按下「顯示標籤」後再顯示|[範例](https://zerojudge.tw/ShowProblem?problemid=j123)
---
+++ zerojudge__hide-tags.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         zerojudge 隱藏標籤
+// @namespace    https://github.com/zica87/self-made-userscipts
+// @version      0.1
+// @description  不顯示題目標籤，按下「顯示標籤」後再顯示
+// @author       zica
+// @match        https://zerojudge.tw/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const tags = document.getElementsByClassName('tag');
+    for(const e of tags){
+        e.hidden = true;
+        const show = document.createElement('button');
+        show.innerText = '顯示標籤';
+        show.onclick = function(){
+            this.nextSibling.hidden = false;
+            this.remove();
+        };
+        e.before(show);
+    }
+})();
