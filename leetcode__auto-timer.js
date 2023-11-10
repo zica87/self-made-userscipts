0a98bcd95013644be6004194143e029189eb43a5
zica <9918800@gmail.com>
2023-11-10 21:48:26 +0800

    [leetcode__auto-timer] fix: adapt to new layout

--- leetcode__auto-timer.js
+++ leetcode__auto-timer.js
@@ -2,7 +2,7 @@
 // @name         Auto start LeetCode timer
 // @name:zh-tw   自動開始 LeetCode 計時器
 // @namespace    https://github.com/zica87/self-made-userscipts
-// @version      1.0
+// @version      1.1
 // @description  Automatically start LeetCode official timer.
 // @description:zh-tw 自動開始 LeetCode 官方計時器。
 // @author       zica
@@ -11,19 +11,22 @@
 // @license      GPL-2.0
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+    "use strict";
 
     if (document.URL.includes("/solutions/")) return;
 
-    const observer = new MutationObserver((records, observerInstance)=>{
-        const timer = document.getElementsByClassName('hover:text-gray-7')[0];
-        if (!timer) return;
-        timer.click();
-        observerInstance.disconnect();
+    const observer = new MutationObserver((_, observerInstance) => {
+        const cur = document.getElementsByClassName("p-2");
+        for (const i of [0, 1]) {
+            if (cur[i]?.classList.contains("flex-none")) {
+                cur[i].click();
+                observerInstance.disconnect();
+            }
+        }
     });
     observer.observe(document.body, {
         childList: true,
-        subtree: true
+        subtree: true,
     });
 })();
